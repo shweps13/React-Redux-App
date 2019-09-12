9974c6c254746db54f7a9021e9565498f4758701
Heorhii Siburov <g.siburov@gmail.com>
2019-09-12 14:08:33 -0700

    API works fine now

--- async-redux/src/components/MainCard.js
+++ async-redux/src/components/MainCard.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
 
 function MainCard() {
+
+const [newRequest, setNewRequest] = useState("");
+console.log(newRequest)
+
+const access_key = "98ccbe4524cb7f7d09e38b4c2a13401a"
+
+const fetchData = () => {
+  axios
+  .put(`http://api.languagelayer.com/detect?access_key=${access_key}&query=${newRequest}`)
+  .then(res => console.log(res))
+  .catch(err =>  console.log(err));
+}
+
+
+const handleUserNameInput = e => {
+  setNewRequest(e.target.value);
+};
+
+const handleChange = e => {
+  setNewRequest(e.target.value);
+  fetchData()
+};
+
+
   return (
     <div className="MainCard">
-        <p>Something important here</p>   
+      <input
+        type="text"
+        onChange={handleUserNameInput}
+        value={newRequest}
+        placeholder="username..."
+      />
+      <button className="btn btn-large right" onClick={handleChange}>
+        Log Names
+      </button>
+
     </div>
   );
 }
+
+
+
 
 export default MainCard;
