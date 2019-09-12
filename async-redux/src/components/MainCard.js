57006fe610cf255eb934259e8eab7aaf12e2c60c
Heorhii Siburov <g.siburov@gmail.com>
2019-09-12 12:37:45 -0700

    App ready for next work

---
+++ async-redux/src/components/MainCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainCard() {
+  return (
+    <div className="MainCard">
+        <p>Something important here</p>   
+    </div>
+  );
+}
+
+export default MainCard;
